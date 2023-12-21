7b51c654cbc1645f9677071adc0b71bdea8f24fb
M. Nur Fawaiq <nurfawaiq@gmail.com>
2023-12-21 23:12:12 +0700

    update support php 8.3.x

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -50,6 +50,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/user_guide/database/
  */
+
+#[\AllowDynamicProperties]
+
 abstract class CI_DB_driver {
 
 	/**
